cef0b93bed1eddfab509566bf54a9a21be4b5683
minhdang2803 <58287394+minhdang2803@users.noreply.github.com>
2022-06-22 09:34:09 +0700

    Done All Features

--- taskist_clone/lib/models/boxes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:hive/hive.dart';
-import 'package:todoist/models/models.dart';
-
-class Boxes {
-  static Box<TaskTables> getTaskTables() =>
-      Hive.box<TaskTables>('taskist_tasktable');
-}
--- taskist_clone/lib/models/models.dart
+++ taskist_clone/lib/models/models.dart
@@ -1,2 +1,1 @@
 export 'tasks.dart';
-export 'boxes.dart';
