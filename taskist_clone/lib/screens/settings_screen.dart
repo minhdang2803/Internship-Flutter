f03094635718624d4525be3e0116a63a11a5f6f9
minhdang2803 <58287394+minhdang2803@users.noreply.github.com>
2022-06-24 09:08:39 +0700

    Add dark mode

--- taskist_clone/lib/screens/settings_screen.dart
+++ taskist_clone/lib/screens/settings_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:todoist/provider/setting_provider.dart';
 import 'package:todoist/screens/webview_screen.dart';
 import 'package:todoist/theme.dart';
 
